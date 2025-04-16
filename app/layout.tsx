0c5717c3e3be56064f3c38c1d258dd182c2784e7
vedu09 <ghadgevedant09@gmail.com>
2025-04-16 11:55:12 +0530

    11

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from '@/lib/constants'
+import { Toaster } from 'sonner'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster richColors position='top-right' />
       </body>
     </html>
   )
--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,16 +1,11 @@
-import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
+import CartButton from './cart-button'
 
 export default function Menu() {
   return (
     <div className='flex justify-end'>
       <nav className='flex gap-3 w-full'>
-        <Link href='/cart' className='header-button'>
-          <div className='flex items-end'>
-            <ShoppingCartIcon className='h-8 w-8' />
-            Cart
-          </div>
-        </Link>
+        <CartButton />
 
         <Link href='/signin' className='flex items-center header-button'>
           Sign in
