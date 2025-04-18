e014c5cacf018027291e33206635b3dd5f921deb
vedu09 <ghadgevedant09@gmail.com>
2025-04-18 13:18:06 +0530

    24

--- app/(root)/account/manage/name/page.tsx
+++ app/(root)/account/manage/name/page.tsx
@@ -3,10 +3,10 @@ import { SessionProvider } from 'next-auth/react'
 
 import { auth } from '@/auth'
 
+import { ProfileForm } from './profile-form'
 import Link from 'next/link'
 import { Card, CardContent } from '@/components/ui/card'
 import { APP_NAME } from '@/lib/constants'
-import { ProfileForm } from '@/app/(root)/account/manage/name/profile-form'
 
 const PAGE_TITLE = 'Change Your Name'
 export const metadata: Metadata = {
