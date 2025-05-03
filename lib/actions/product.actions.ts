8334005612e4861c63570becf7faff3d68ab8da2
vedu09 <ghadgevedant09@gmail.com>
2025-05-03 12:28:12 +0530

    29

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -5,6 +5,9 @@ import Product, { IProduct } from '@/lib/db/models/product.model'
 import { PAGE_SIZE } from '../constants'
 import { revalidatePath } from 'next/cache'
 import { formatError } from '../utils'
+import { IProductInput } from '@/types'
+import { ProductInputSchema, ProductUpdateSchema } from '../validator'
+import { z } from 'zod'
 
 export async function getAllCategories() {
   await connectToDatabase()
@@ -273,4 +276,42 @@ export async function getAllProductsForAdmin({
     from: pageSize * (Number(page) - 1) + 1,
     to: pageSize * (Number(page) - 1) + products.length,
   }
+}
+
+// CREATE
+export async function createProduct(data: IProductInput) {
+  try {
+    const product = ProductInputSchema.parse(data)
+    await connectToDatabase()
+    await Product.create(product)
+    revalidatePath('/admin/products')
+    return {
+      success: true,
+      message: 'Product created successfully',
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+// UPDATE
+export async function updateProduct(data: z.infer<typeof ProductUpdateSchema>) {
+  try {
+    const product = ProductUpdateSchema.parse(data)
+    await connectToDatabase()
+    await Product.findByIdAndUpdate(product._id, product)
+    revalidatePath('/admin/products')
+    return {
+      success: true,
+      message: 'Product updated successfully',
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+// GET ONE PRODUCT BY ID
+export async function getProductById(productId: string) {
+  await connectToDatabase()
+  const product = await Product.findById(productId)
+  return JSON.parse(JSON.stringify(product)) as IProduct
 }
--- lib/validator.ts
+++ lib/validator.ts
@@ -188,3 +188,7 @@ export const UserSignUpSchema = UserSignInSchema.extend({
 export const UserNameSchema = z.object({
   name: UserName,
 })
+
+export const ProductUpdateSchema = ProductInputSchema.extend({
+  _id: z.string(),
+})
